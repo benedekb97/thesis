c62db6238bfaa394a3dbd44975862f6be7775acf
Benedek Burgess <benedekb97@gmail.com>
2021-10-27 10:49:28 +0200

    Add jquery, bootstrap, popper.js and dependencies to package.json

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,9 +2,11 @@
 <html lang="hu">
     <head>
         <meta charset="UTF-8"/>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}"/>
         <title>@yield('title')</title>
     </head>
     <body>
         @yield('content')
     </body>
+    <script src="{{ asset('js/app.js') }}"></script>
 </html>
